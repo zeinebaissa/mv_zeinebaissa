af5c9805c85713c7fec072a1821588941eb0cb88
zeinebaissa <127880125+zeinebaissa@users.noreply.github.com>
2024-04-22 09:38:30 +0100

    Installing Doctrine: symfony/orm-pack

---
+++ src/Controller/SongController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SongController extends AbstractController
+{
+    #[Route('/api/songs/{id<\d+>}', methods: ['GET'], name: 'api_songs_get_one')]
+    function getSong(int $id): Response
+    {
+        // TODO query the database
+        $song = [
+            'id' => $id,
+            'name' => 'Waterfalls',
+            'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
+        ];
+        return $this->json($song);
+    }
+}
