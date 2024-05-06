a102ec6422c723cbe349b872655be687190ccda0
zeinebaissa <zeinebaissa2003@gmail.com>
2024-05-06 09:28:41 +0100

    "Querying for a Single Entity for a "Show" Page"

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -33,16 +33,12 @@ class MixController extends AbstractController
     }
 
     #[Route('/mix/{id}', name: 'app_mix_show')]
-    public function show($id, VinylMixRepository $mixRepository): Response
+    public function show(VinylMix $mix): Response
     {
-        $mix = $mixRepository->find($id);
-
-        if (!$mix) {
-            throw $this->createNotFoundException('Mix not found');
-        }
-
         return $this->render('mix/show.html.twig', [
-            'mix' => $mix,
+           'mix' => $mix,
         ]);
     }
+
+
 }
