edf09dde9e1b061277527e1cd147f46879901d6a
zeinebaissa <zeinebaissa2003@gmail.com>
2024-05-13 12:53:18 +0100

    Foundry: Fixtures You'll Love

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
